6d7fde4fc9c9e1e5e6a8de33e50541fd27f62065
arnav <arnav@192.168.1.239>
2023-02-18 11:20:11 -0800

    almost done mod4

--- src/_99_extra/hide_the_creeper/HideTheCreeper.java
+++ src/_99_extra/hide_the_creeper/HideTheCreeper.java
@@ -1,23 +1,32 @@
 package _99_extra.hide_the_creeper;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class HideTheCreeper extends PApplet {
     static final int WIDTH = 600;
     static final int HEIGHT = 400;
+    
+    PImage creeper;
 
     @Override
     public void settings() {
-        size(WIDTH, HEIGHT);
+        size(500, 500);
     }
 
     @Override
     public void setup() {
-        
+    	PImage minecraft = loadImage("minecraft.png");     //in setup method
+    	minecraft.resize(500, 500);          //in setup method
+    	background(minecraft);          //in setup method
+    	
+    	creeper=loadImage("creeper.png");
+    	creeper.resize(100, 50);
     }
 
     @Override
     public void draw() {
+    	
         
     }
 
